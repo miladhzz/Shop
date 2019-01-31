f8a914ba2a7b05e58e7bb81006bb43f7820c1fa0
Milad Hatami <miladhzz@gmail.com>
2019-01-31 07:35:25 +0330

    add category, Tag and Image Gallery models.
    next step complete models

--- mysite/shop/admin.py
+++ mysite/shop/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from shop.models import Category, Tag, Image_gallery
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Image_gallery)
--- mysite/shop/models.py
+++ mysite/shop/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(max_length=500, null=True, blank=True)
+    parentCategory = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True,
+                                       verbose_name="Parent Category")
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    description = models.TextField(max_length=500, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Image_gallery(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    pic = models.ImageField(upload_to='media/upload/images', default='media/upload/images/no-img.jpg')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Image Gallery'
+        verbose_name_plural = 'Image Gallery'
