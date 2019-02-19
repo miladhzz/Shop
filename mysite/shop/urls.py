5b8fee8aa17604a838da48ae41005be0bcfc699e
Milad Hatami <miladhzz@gmail.com>
2019-02-19 07:18:12 +0330

    fix media location settings

--- mysite/shop/urls.py
+++ mysite/shop/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from shop import views
 
@@ -7,4 +9,4 @@ urlpatterns = [
     path('product/', views.Product.as_view(), name='product'),
     path('cart/', views.Cart.as_view(), name='cart'),
     path('checkout/', views.Checkout.as_view(), name='checkout'),
-    ]
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
