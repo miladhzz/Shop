0bbec5ad11ae18b62e71c286972d6f21ccb68d59
Milad Hatami <miladhzz@gmail.com>
2019-02-27 06:30:01 +0330

    create mixin insted of model function to display latest product
    next step complete templates

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -2,27 +2,32 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views import View
 from .models import Product
+from django.views.generic.base import ContextMixin
 
 
-# Create your views here
+class Product_Mixin(ContextMixin):
+    def get_context_data(self, *args, **kwargs):
+        ctx = super(Product_Mixin, self).get_context_data(**kwargs)
+        ctx['latest_product'] = Product.objects.all()[:5]
+        return ctx
+
+
 class Product_list(ListView):
     model = Product
     template_name = 'product_list.html'
 
 
-class Product_detail(DetailView):
+class Product_detail(Product_Mixin, DetailView):
     model = Product
     template_name = 'product_detail.html'
 
 
 class Home(View):
-
     def get(self, request):
         return render(request, 'home.html')
 
 
 class Cart(View):
-
     def get(self, request):
         return render(request, 'cart.html')
 
