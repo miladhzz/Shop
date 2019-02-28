2adf416f8c2182e33a2830685bf8217093c5edba
Milad Hatami <miladhzz@gmail.com>
2019-02-28 07:25:06 +0330

    complete templates
    next step add to cart

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -8,7 +8,9 @@ from django.views.generic.base import ContextMixin
 class Product_Mixin(ContextMixin):
     def get_context_data(self, *args, **kwargs):
         ctx = super(Product_Mixin, self).get_context_data(**kwargs)
-        ctx['latest_product'] = Product.objects.all()[:5]
+        ctx['latest_product'] = Product.objects.all()[:6]
+        ctx['recently_view'] = Product.objects.all()[3:9]
+        ctx['top_sel'] = Product.objects.all()[9:12]
         return ctx
 
 
@@ -22,9 +24,9 @@ class Product_detail(Product_Mixin, DetailView):
     template_name = 'product_detail.html'
 
 
-class Home(View):
-    def get(self, request):
-        return render(request, 'home.html')
+class Home(Product_Mixin, ListView):
+    model = Product
+    template_name = "home.html"
 
 
 class Cart(View):
