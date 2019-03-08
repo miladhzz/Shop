b49456be647c5f00d0e79d4a236b1a9f68955722
Milad Hatami <miladhzz@gmail.com>
2019-03-08 15:36:38 +0330

    Separate mixins, complete cart detail, add 'add to cart' button for all pages

--- mysite/shop/forms.py
+++ mysite/shop/forms.py
@@ -5,16 +5,12 @@ PRODUCT_COUNT_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 class CartAddProductForm(forms.Form):
     count = forms.TypedChoiceField(choices=PRODUCT_COUNT_CHOICES,
-                                   coerce=int)
-    update = forms.BooleanField(required=False,
-                                initial=False,
-                                widget=forms.HiddenInput)
-
-
-class CartAddProductFormSingle(forms.Form):
-    count = forms.TypedChoiceField(choices=PRODUCT_COUNT_CHOICES,
                                    coerce=int,
-                                   widget=forms.HiddenInput)
+                                   initial=1,
+                                   )
     update = forms.BooleanField(required=False,
                                 initial=False,
                                 widget=forms.HiddenInput)
+
+
+
--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -4,32 +4,49 @@ from django.views import View
 from .models import Product
 from django.views.generic.base import ContextMixin
 from django.views.decorators.http import require_POST
-from .forms import CartAddProductForm, CartAddProductFormSingle
+from .forms import CartAddProductForm
 from .cart import Cart
 
 
-class Product_Mixin(ContextMixin):
+class Form_Mixin(ContextMixin):
     def get_context_data(self, *args, **kwargs):
-        ctx = super(Product_Mixin, self).get_context_data(**kwargs)
-        ctx['latest_product'] = Product.objects.all()[:6]
-        ctx['recently_view'] = Product.objects.all()[3:9]
-        ctx['top_sel'] = Product.objects.all()[9:12]
+        ctx = super(Form_Mixin, self).get_context_data(**kwargs)
         ctx['cart_product_from'] = CartAddProductForm()
-        ctx['cart_product_from_single'] = CartAddProductFormSingle()
         return ctx
 
 
-class Product_list(ListView):
+class TopSel_Mixin(ContextMixin):
+    def get_context_data(self, *args, **kwargs):
+        ctx = super(TopSel_Mixin, self).get_context_data(**kwargs)
+        ctx['top_sel'] = Product.objects.all()[9:12]
+        return ctx
+
+
+class Recently_Mixin(ContextMixin):
+    def get_context_data(self, *args, **kwargs):
+        ctx = super(Recently_Mixin, self).get_context_data(**kwargs)
+        ctx['recently_view'] = Product.objects.all()[3:9]
+        return ctx
+
+
+class Latest_Mixin(ContextMixin):
+    def get_context_data(self, *args, **kwargs):
+        ctx = super(Latest_Mixin, self).get_context_data(**kwargs)
+        ctx['latest_product'] = Product.objects.all()[3:9]
+        return ctx
+
+
+class Product_list(Form_Mixin, ListView):
     model = Product
     template_name = 'product_list.html'
 
 
-class Product_detail(Product_Mixin, DetailView):
+class Product_detail(TopSel_Mixin, Latest_Mixin, Form_Mixin, DetailView):
     model = Product
     template_name = 'product_detail.html'
 
 
-class Home(Product_Mixin, ListView):
+class Home(TopSel_Mixin, Recently_Mixin, Latest_Mixin,Form_Mixin, ListView):
     model = Product
     template_name = "home.html"
 
@@ -51,6 +68,20 @@ def cart_add(request, product_id):
                  count=cd['count'],
                  update_count=cd['update'])
     return redirect('cart_detail')
+
+'''
+@require_POST
+def cart_add_single(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductFormSingle(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product,
+                 count=cd['count'],
+                 update_count=cd['update'])
+    return redirect('cart_detail')
+'''
 
 
 def cart_remove(request, product_id):
